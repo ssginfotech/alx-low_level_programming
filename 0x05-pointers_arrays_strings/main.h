53ee5de4b50a4ecbfbb05b6588efb9f81d44c8bc
ssginfotech <djssg2022@gmail.com>
2022-09-19 17:48:44 -0700

    array

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,15 +2,15 @@
 #define MAIN_H
 
 void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-int _putchar(char);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
+int _atoi(char *s);
 char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
 
 #endif
