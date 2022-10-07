00b2617188ce5ee4ec89c388de862158370aa396
ssginfotech <djssg2022@gmail.com>
2022-10-07 09:30:57 -0700

    concate strings

---
+++ 0x0C-more_malloc_free/1-string_nconcate.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - COncatenates two strings using at
+ * most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
+ *
+ * Return: If the function fails - NULL.
+ * Otherwise - a pointer to the concatenated space in memory.
+ */
+char
